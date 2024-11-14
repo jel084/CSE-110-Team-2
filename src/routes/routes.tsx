e7bfc441d9b7339040a48bb756192fa09a224771
ZQSam <131108692+ZQSam@users.noreply.github.com>
2024-11-14 10:41:48 -0800

    move routes.tsx to routes folder

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,14 @@
-import { ROUTES } from "./constants/routes";
+import { ROUTES } from "../constants/routes";
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import NotFoundPage from "./pages/ErrorPage/NotFoundPage";
-import PinPage from "./pages/PinPage/PinPage";
-import CreateGamePage from "./pages/CreateGamePage/CreateGamePage";
-import LobbyPage from "./pages/LobbyPage/LobbyPage";
-import ScavengerPage from "./pages/ScavengerPage.tsx/ScavengerPage";
-import TimeUpPage from "./pages/TimeUpPage/TimeUpPage";
-import WinningPage from "./pages/WinningPage/WinningPage";
+import HomePage from "../pages/HomePage/HomePage";
+import NotFoundPage from "../pages/ErrorPage/NotFoundPage";
+import PinPage from "../pages/PinPage/PinPage";
+import CreateGamePage from "../pages/CreateGamePage/CreateGamePage";
+import LobbyPage from "../pages/LobbyPage/LobbyPage";
+import ScavengerPage from "../pages/ScavengerPage.tsx/ScavengerPage";
+import TimeUpPage from "../pages/TimeUpPage/TimeUpPage";
+import WinningPage from "../pages/WinningPage/WinningPage";
 
 export const router = createBrowserRouter([
   {
