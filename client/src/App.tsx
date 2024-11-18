509c83b7764505c8add86bdc85b98e86e8bb188e
anc034 <chavez21andre@gmail.com>
2024-11-18 02:55:00 -0800

    removed obsolete file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AppProvider } from './context/AppContext';
-import { ScavengerApp } from './ScavengerApp';
 import './App.css';
 import ScoreScreen from './pages/WinningPage/scoreScreen';
 import HomePage from './pages/HomePage/HomePage';
