12d876008ab848c66ec3e4788de87c1dade10f79
prestondntran <prestondnt@gmail.com>
2024-11-19 20:14:12 -0800

    Added frontend and backend tests for host and join

--- client/src/constants/constants.tsx
+++
@@ -1,5 +0,0 @@
-export const testItems = [
-    {id : 1, name : "Item 1", points : 10, found : false},
-    {id : 2, name : "Item 2", points : 20, found : false},
-]
-export const API_BASE_URL = "http://localhost:8080";
