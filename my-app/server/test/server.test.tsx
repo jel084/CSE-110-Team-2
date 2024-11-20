8663aa7905819c504b9275fcb9d1a7d6ae46f59e
prestondntran <prestondnt@gmail.com>
2024-11-19 19:58:40 -0800

    Added frontend and backend tests for host and join

---
+++ my-app/server/test/server.test.tsx
@@ -0,0 +1,137 @@
+import axios from 'axios';
+import app from '../server';
+import { connectDB } from '../db';
+import { initDatabase } from '../db_table';
+import { Database } from 'sqlite';
+import { Server } from 'http';
+
+let server: Server;
+let db: Database;
+const fs = require('fs');
+const PORT = process.env.PORT || 8080;
+
+beforeAll(async () => {
+    db = await connectDB();
+    await initDatabase();
+    server = await app.listen(PORT); // Start the server before tests
+});
+
+afterAll(async () => {
+    // Delete all created entries just in case not all tests run
+    await db.run(`
+      DELETE FROM lobbies WHERE (lobbyName = 'New Lobby 1' OR lobbyName = 'New Lobby 2');
+    `);
+
+    // Automatically delete database.sqlite if it was created right before testing
+    const result = await db.get('SELECT COUNT(*) AS count FROM lobbies');
+    if (result.count === 0)
+      fs.unlinkSync('database.sqlite');
+
+    server.close(); // Stop the server after tests
+});
+
+describe('Lobby Endpoints', () => {
+
+  test('GET /lobbies should show all lobbies', async () => {
+    await db.run(`
+        INSERT INTO lobbies (lobbyName, host, players, scavengerItems, points, pin, status) VALUES
+        ('New Lobby 1', 'Host 1', '[]', '[]', '[]', '1234', 'waiting'),
+        ('New Lobby 2', 'Host 2', '["Player 1","Player 2"]', '[{"id":1,"name":"Triton Statue","points":20,"found":false}]', 
+        '[{"id":"Player 1","points":0},{"id":"Player 2","points":0}]', '5678', 'in-progress')
+    `);
+
+    // Perform the GET request to the /lobbies endpoint
+    const res = await axios.get(`http://localhost:${PORT}/api/lobbies`);
+
+    expect(res.status).toBe(200);
+    expect(res.data).toHaveLength(2);
+    expect(res.data[0]).toMatchObject({
+        lobbyName: 'New Lobby 1',
+        host: 'Host 1',
+        players: '[]',
+        scavengerItems: '[]',
+        points: '[]',
+        pin: '1234',
+        status: 'waiting',
+    });
+    expect(res.data[1]).toMatchObject({
+      lobbyName: 'New Lobby 2',
+      host: 'Host 2',
+      players: `["Player 1","Player 2"]`,
+      scavengerItems: `[{"id":1,"name":"Triton Statue","points":20,"found":false}]`,
+      points: '[{"id":"Player 1","points":0},{"id":"Player 2","points":0}]',
+      pin: '5678',
+      status: 'in-progress',
+    });
+
+    await db.run(`
+      DELETE FROM lobbies WHERE (lobbyName = 'New Lobby 1' OR lobbyName = 'New Lobby 2');
+    `);
+  });
+
+  test('POST /create should add a new lobby to lobbies', async () => {
+    // Perform the POST request to the /create endpoint
+    const res = await axios.post(`http://localhost:${PORT}/api/create`, {
+      lobbyName: 'New Lobby 1',
+      scavengerItems: [{id: 1, name: "Triton Statue", points: 20, found: false}, {id: 2, name: "Sun God", points: 20, found: false}],
+      userId: 'Host 1',
+      pin: '1234'
+    });
+
+    expect(res.status).toBe(201);
+    expect(res.data.message).toBe("Lobby 'New Lobby 1' created by Host 1");
+
+    const lobby_res = await axios.get(`http://localhost:${PORT}/api/lobbies`);
+
+    expect(lobby_res.status).toBe(200);
+    expect(lobby_res.data).toHaveLength(1);
+    expect(lobby_res.data[0]).toMatchObject({
+        lobbyName: 'New Lobby 1',
+        host: 'Host 1',
+        players: '[]',
+        scavengerItems: `[{"id":1,"name":"Triton Statue","points":20,"found":false},{"id":2,"name":"Sun God","points":20,"found":false}]`,
+        points: '[]',
+        pin: '1234',
+        status: 'waiting'
+    });
+
+    await db.run(`
+      DELETE FROM lobbies WHERE (lobbyName = 'New Lobby 1');
+    `);
+  });
+
+  test('POST /join should add a player to the given lobby', async () => {
+    // Create a lobby for the player to join
+    const create_res = await axios.post(`http://localhost:${PORT}/api/create`, {
+      lobbyName: 'New Lobby 1',
+      scavengerItems: [{id: 1, name: "Triton Statue", points: 20, found: false}, {id: 2, name: "Sun God", points: 20, found: false}],
+      userId: 'Host 1',
+      pin: '1234'
+    });
+
+    // Perform the POST request to the /join endpoint
+    const res = await axios.post(`http://localhost:${PORT}/api/join`, {
+      lobbyId: 4,
+      userId: 'Preston',
+      pin: '1234'
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.data.message).toBe("User Preston joined lobby 4");
+
+    const lobby_res = await axios.get(`http://localhost:${PORT}/api/lobbies`);
+
+    expect(lobby_res.status).toBe(200);
+    expect(lobby_res.data).toHaveLength(1);
+    expect(lobby_res.data[0]).toMatchObject({
+        lobbyName: 'New Lobby 1',
+        host: 'Host 1',
+        players: '["Preston"]',
+        scavengerItems: `[{"id":1,"name":"Triton Statue","points":20,"found":false},{"id":2,"name":"Sun God","points":20,"found":false}]`,
+        points: '[{"id":"Preston","points":0}]',
+        pin: '1234',
+        status: 'waiting'
+    });
+  });
+  
+  });
