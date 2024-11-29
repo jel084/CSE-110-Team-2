15784a75a6f2d9545060a04e59204882b4669608
ZQSam <131108692+ZQSam@users.noreply.github.com>
2024-11-29 14:26:03 -0800

    add go back button to PinPage with new type

--- my-app/client/src/types/custom.d.ts
+++ my-app/client/src/types/custom.d.ts
@@ -2,3 +2,16 @@ declare module "*.png" {
     const value: string;
     export default value;
 }
+
+declare namespace JSX {
+    interface IntrinsicElements {
+      'box-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        name?: string;
+        type?: string;
+        color?: string;
+        size?: string;
+        animation?: string;
+      };
+    }
+  }
+  
